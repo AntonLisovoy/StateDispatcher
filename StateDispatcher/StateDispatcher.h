644e4abcbc0215e2f96ea22a41488f19c7eec2c5
Anton Lisovoy <lisovoy@arcsinus.ru>
2020-07-04 14:12:19 +0300

    Initial Commit

---
+++ StateDispatcher/StateDispatcher.h
@@ -0,0 +1,19 @@
+//
+//  StateDispatcher.h
+//  StateDispatcher
+//
+//  Created by Anton Lisovoy on 04.07.2020.
+//  Copyright © 2020 Anton Lisovoy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StateDispatcher.
+FOUNDATION_EXPORT double StateDispatcherVersionNumber;
+
+//! Project version string for StateDispatcher.
+FOUNDATION_EXPORT const unsigned char StateDispatcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StateDispatcher/PublicHeader.h>
+
+
---
+++ StateDispatcherTests/StateDispatcherTests.swift
@@ -0,0 +1,34 @@
+//
+//  StateDispatcherTests.swift
+//  StateDispatcherTests
+//
+//  Created by Anton Lisovoy on 04.07.2020.
+//  Copyright © 2020 Anton Lisovoy. All rights reserved.
+//
+
+import XCTest
+@testable import StateDispatcher
+
+class StateDispatcherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
